8206ad26fd5a2e26babbb421b56a1876dd3d672d
Peter Dias <peterdias77@gmail.com>
2022-05-29 16:27:48 +0000

    update

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -2,21 +2,31 @@ const asyncHandler = require('express-async-handler')
 const Influx = require('influx');
 
 const getBars = asyncHandler(async (req, res) => {
-     
+    const {symbol, tf, start,stop,limit } = req.body 
+
+    // if (!symbol || !tf || !start || !stop) {
+    //   res.status(400)
+    //   throw new Error('Some fields are missing')
+    // }
+
     const influx = new Influx.InfluxDB({
-        host: '10.122.0.4',
-        database: 'databank'        
+        host: process.env.IDB_HOST,
+        database: process.env.IDB_DATABASE        
       });
     
-    influx.query(`
-      select * from bars
-      limit 5
-    `)
-    .then( result => res.status(200).json(result) )
+    const query = "select time,open,high,low,close,vol from bars limit 2"
+    influx.query(query)
+    .then( rows => { 
+      var output = new Array();
+
+      rows.forEach(row => {        
+        output.push([row.time.getNanoTime(),row.open,row.high,row.low,row.close,row.vol]);
+      })
+      res.status(200).json(output.reverse()) 
+    } 
+    )
     .catch( error => res.status(500).json({ error }) );
-
-    
-
+   
     return({})
   })
 
