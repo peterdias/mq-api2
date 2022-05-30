147d16cf4bf26980884728d72aa917792f752897
Peter Dias <peterdias77@gmail.com>
2022-05-30 03:40:48 +0000

    update

--- config/db.js
+++ config/db.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI)
+
+    const uri = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
+    const conn = await mongoose.connect(uri)
 
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
   } catch (error) {
