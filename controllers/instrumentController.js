3fc7999830c89c76d8b7045020d17113c0d76823
Peter Dias <peterdias77@gmail.com>
2022-07-07 14:16:17 +0530

    update

--- controllers/instrumentController.js
+++ controllers/instrumentController.js
@@ -3,19 +3,19 @@ const Instrument = require('../models/instrument')
  
 const getInstruments = asyncHandler(async (req, res) => {
   const { exchange, segment } = req.body
-  
-  let filter  = {}
-  if(exchange) filter.exchange = exchange
-  if(segment) filter.segment = exchange
-   
-  const instruments = await Instrument.find(filter)
+    console.log("inside getInstrumentsController")
+    let filter  = {}
+    if(exchange) filter.exchange = exchange
+    if(segment) filter.segment = exchange
+    
+    const instruments = await Instrument.find(filter)
 
-  if (instruments) {
-    res.status(201).json(instruments)
-  } else {
-    res.status(400)
-    throw new Error('Error fetching Instruments')
-  }
+    if (instruments) {
+      res.status(201).json(instruments)
+    } else {
+      res.status(400)
+      throw new Error('Error fetching Instruments')
+    }
 })
 
 
