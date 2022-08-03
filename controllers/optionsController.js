2c3b3eb41859d07916ac0cd3bd7ed1ec26fbabec
Peter Dias <peterdias77@gmail.com>
2022-08-03 17:23:05 +0530

    fix

--- controllers/optionsController.js
+++ controllers/optionsController.js
@@ -80,7 +80,7 @@ async function loadInstruments()
 async function getSpotPrices()
 {    
     let promise = new Promise((resolve,reject)=>{ 
-        let k =  spot_exchange+":"+spot_symbol
+        let k =  trim(spot_exchange+":"+spot_symbol)
         console.log("Key:", k)   
         kiteConnect.getQuote([k]).then(data => { 
                 console.log(data)
