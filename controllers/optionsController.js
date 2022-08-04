eea707670394905219f1b6c1eaab078972ea2b72
Peter Dias <peterdias77@gmail.com>
2022-08-04 17:47:19 +0530

    iv

--- controllers/optionsController.js
+++ controllers/optionsController.js
@@ -132,11 +132,15 @@ async function calculateChain()
         
         let c = Object.values(quotes).find(el => el.instrument_token == call[0].instrument_token);
         let p = Object.values(quotes).find(el => el.instrument_token == put[0].instrument_token);
-                  
+        
+        let days = Math.round((call[0].expiry - new Date() )/(1000*60*60*24))        
+        let civ = option_implied_volatility(true,spot_price,i,10/100,days/365, c.last_price)  
+        let piv = option_implied_volatility(false,spot_price,i,10/100,days/365, p.last_price)  
+
         let o = {
                 ce_oi: c.oi,
                 ce_vol: c.last_quantity,
-                ce_iv: 0,                                
+                ce_iv: civ,                                
                 ce_ltp: c.last_price, 
                 ce_chng: c.net_change,
                 ce_bid: c.depth.buy[0].price,
@@ -146,7 +150,7 @@ async function calculateChain()
                 strike: i,              
                 pe_oi: p.oi,
                 pe_vol: p.last_quantity,
-                pe_iv: 0,                                
+                pe_iv: piv,                                
                 pe_ltp: p.last_price, 
                 pe_chng: p.net_change,
                 pe_bid: p.depth.buy[0].price,
@@ -239,6 +243,90 @@ const getExpiryDates = asyncHandler(async (req, res) => {
     }    
 })
 
+////////////////////////////////////
+function ndist(z) {
+    return (1.0/(Math.sqrt(2*Math.PI)))*Math.exp(-0.5*z);
+    //??  Math.exp(-0.5*z*z)
+  }
+   
+  function N(z) {
+    b1 =  0.31938153;
+    b2 = -0.356563782;
+    b3 =  1.781477937;
+    b4 = -1.821255978;
+    b5 =  1.330274429;
+    p  =  0.2316419;
+    c2 =  0.3989423;
+    a=Math.abs(z);
+    if (a>6.0) {return 1.0;} 
+    t = 1.0/(1.0+a*p);
+    b = c2*Math.exp((-z)*(z/2.0));
+    n = ((((b5*t+b4)*t+b3)*t+b2)*t+b1)*t;
+    n = 1.0-b*n;
+    if (z < 0.0) {n = 1.0 - n;}
+    return n;
+  }  
+    
+  function black_scholes(call,S,X,r,v,t) { 
+  // call = Boolean (to calc call, call=True, put: call=false)
+  // S = stock prics, X = strike price, r = no-risk interest rate
+  // v = volitility (1 std dev of S for (1 yr? 1 month?, you pick)
+  // t = time to maturity
+   
+  // define some temp vars, to minimize function calls
+    var sqt = Math.sqrt(t);
+    var Nd2;  //N(d2), used often
+    var nd1;  //n(d1), also used often
+    var ert;  //e(-rt), ditto
+    var delta;  //The delta of the option
+   
+    d1 = (Math.log(S/X) + r*t)/(v*sqt) + 0.5*(v*sqt);
+    d2 = d1 - (v*sqt);
+   
+    if (call) {
+      delta = N(d1);
+      Nd2 = N(d2);
+    } else { //put
+      delta = -N(-d1);
+      Nd2 = -N(-d2);
+    }
+   
+    ert = Math.exp(-r*t);
+    nd1 = ndist(d1);
+   
+    gamma = nd1/(S*v*sqt);
+    vega = S*sqt*nd1;
+    theta = -(S*v*nd1)/(2*sqt) - r*X*ert*Nd2;
+    rho = X*t*ert*Nd2;
+   
+    return ( S*delta-X*ert *Nd2);
+   
+  } //end of black_scholes
+   
+  function option_implied_volatility(call,S,X,r,t,o) { 
+  // call = Boolean (to calc call, call=True, put: call=false)
+  // S = stock prics, X = strike price, r = no-risk interest rate
+  // t = time to maturity
+  // o = option price
+   
+  // define some temp vars, to minimize function calls
+    sqt = Math.sqrt(t);
+    MAX_ITER = 100;
+    ACC = 0.0001;
+   
+    sigma = (o/S)/(0.398*sqt);
+    for (i=0;i<MAX_ITER;i++) {
+      let price = black_scholes(call,S,X,r,sigma,t);
+      let ddiff = o-price;
+      if (Math.abs(ddiff) < ACC) return sigma;
+      d1 = (Math.log(S/X) + r*t)/(sigma*sqt) + 0.5*sigma*sqt;
+      vega = S*sqt*ndist(d1);
+      sigma = sigma+ddiff/vega;
+    }
+    return 0;   
+  } 
+
+/////////////////////////////////////
 module.exports = {
     getChain,
     getExpiryDates
