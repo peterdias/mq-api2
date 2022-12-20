a4ad75c1520df54b5c873538fae773e20ca46e31
Peter Dias <peterdias77@gmail.com>
2022-12-20 14:57:08 +0530

    update

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,9 @@ const asyncHandler = require('express-async-handler')
 const protect = asyncHandler(async (req, res, next) => {
     const token = req.headers.authorization.split(' ')[1];
     try {
+        console.log(token)
         const decodeValue = await admin.auth().verifyIdToken(token);
+        console.log(decodeValue)
         if (decodeValue) {
             req.user = decodeValue;
             return next();
