3f081f0587fc0656ee034d26e555260fc19ecf69
Peter Dias <peterdias77@gmail.com>
2023-02-11 18:43:09 +0530

    fix firebase

--- config/firebase.js
+++ config/firebase.js
@@ -3,7 +3,7 @@ const firebaseadmin = require('firebase-admin');
 const serviceAccount = require('./firebaseserviceaccount.json');
 
 firebaseadmin.initializeApp({
-	credential: admin.credential.cert(serviceAccount),
+	credential: firebaseadmin.credential.cert(serviceAccount),
 });
 
 module.exports = firebaseadmin;
