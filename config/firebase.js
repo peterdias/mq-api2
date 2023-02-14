95b4bedd6ece496f12581484dcd416990376d785
Peter Dias <peterdias77@gmail.com>
2023-02-14 18:28:10 +0530

    fix

--- config/firebase.js
+++ config/firebase.js
@@ -6,25 +6,25 @@ firebaseadmin.initializeApp({
 	credential: firebaseadmin.credential.cert(serviceAccount),
 });
 
-const getAllUsers = async () => {
-	firebaseadmin.auth().listUsers(1000).then((listUsersResult) => { 
-		let users = []
-		listUsersResult.users.forEach(user => {        
-		  users.push({
-			'id': user.uid,
-			'email': user.email,
-			'displayName': user.displayName,
-			'creationTime' : user.metadata.creationTime,
-			'lastSignInTime': user.metadata.lastSignInTime,
-			//'providerId' : user.providerData[0].UserInfo.providerId
-		  })
-		});      
+// const getAllUsers = async () => {
+// 	firebaseadmin.auth().listUsers(1000).then((listUsersResult) => { 
+// 		let users = []
+// 		listUsersResult.users.forEach(user => {        
+// 		  users.push({
+// 			'id': user.uid,
+// 			'email': user.email,
+// 			'displayName': user.displayName,
+// 			'creationTime' : user.metadata.creationTime,
+// 			'lastSignInTime': user.metadata.lastSignInTime,
+// 			//'providerId' : user.providerData[0].UserInfo.providerId
+// 		  })
+// 		});      
 		
-		return users 
-	})
-	.catch(function (error) {
-		return []
-	});
-  }
+// 		return users 
+// 	})
+// 	.catch(function (error) {
+// 		return []
+// 	});
+//   }
 
-module.exports = {firebaseadmin, getAllUsers};
+module.exports = firebaseadmin;
